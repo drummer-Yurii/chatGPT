e4d03ca8c218ed6fc923590739c136a30c935146
Yurii Rybachok <drummer2009@ukr.net>
2023-03-12 13:49:53 +0200

    add chat component & setup chat engine

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
-function App() {
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Chat from '@/components/chat';
 
+function App() {
   return (
     <div className="app">
-      app
+      <BrowserRouter>
+        <Routes>
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
