546ae4bcdd6cbe710a16e6e593f978ace99fdd30
Yurii Rybachok <drummer2009@ukr.net>
2023-03-12 18:39:58 +0200

    chat custom message form

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from 'react-chat-engine-advanced';
 import Header from "@/components/customHeader";
+import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm";
 
 const Chat = () => {
   const chatProps = useMultiChatLogic(import.meta.env.VITE_PROJECT_ID, 'testuser', '1234');
@@ -12,6 +13,11 @@ const Chat = () => {
         {...chatProps}
         style={{ height: '100vh' }}
         renderChatHeader={(chat) => <Header chat={chat} />}
+        renderMessageForm={(props) => {
+            return (
+                <StandardMessageForm props={props} activeChat={chatProps.chat} />
+            )
+        }}
       />
     </div>
   );
